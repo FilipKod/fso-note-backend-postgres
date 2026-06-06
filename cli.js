59db46b94dc3ba90abf922a84f5d676ffaf61c96
Filip Madunicky <yelidric@gmail.com>
2026-06-06 12:16:31 +0200

    feat: implement Express server with CRUD operations for blogs

--- cli.js
+++ cli.js
@@ -1,38 +1,9 @@
 require("dotenv").config({quiet: true})
-const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize")
+const { Sequelize, QueryTypes } = require("sequelize")
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'postgres',
   ssl: false
-})
-
-class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT,
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
 })
 
 const main = async () => {
