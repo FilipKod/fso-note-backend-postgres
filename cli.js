76e82993a8a1d1608c9b5cf8ae7efa41024ce0ca
Filip Madunicky <yelidric@gmail.com>
2026-06-06 09:39:14 +0200

    chore: initialize package.json with dependencies and project metadata

---
+++ cli.js
@@ -0,0 +1,51 @@
+require("dotenv").config({quiet: true})
+const { Sequelize, Model, DataTypes, QueryTypes } = require("sequelize")
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+  dialect: 'postgres',
+  ssl: false
+})
+
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT,
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+const main = async () => {
+  try {
+    await sequelize.authenticate({logging: false})
+    const blogs = await sequelize.query("SELECT * FROM blogs", {type: QueryTypes.SELECT})
+    blogs.map(blog => {
+      console.log(`${blog.author || "anonym"}: '${blog.title}', ${blog.likes} likes`)
+    })
+    sequelize.close()
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+}
+
+main()
