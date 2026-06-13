445d86885a28a1aed46fdb1bc2a6e46a6f1b0f17
Filip Madunicky <yelidric@gmail.com>
2026-06-13 13:31:33 +0200

    fix: correct database URL environment variable in configuration

--- util/config.js
+++ util/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config({quiet: true})
 
 module.exports = {
-  DB_URL: process.env.DB_URL,
+  DB_URL: process.env.DATABASE_URL,
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
   PORT: process.env.PORT || 3001,
   JWT_SERCET: process.env.SECRET,
